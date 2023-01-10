875d1d3fa3584bd55789e0e4d88f7340d154eb23
Jerome F <jerome.folger@konvert.ch>
2023-01-10 17:14:58 +0100

    added route, controller and vue page for artikel

---
+++ app/Http/Controllers/ArtikelController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+use Inertia\Response;
+use App\Models\Artikel;
+
+class ArtikelController extends Controller
+{
+
+    /**
+     * Delete the user's account.
+     */
+    public function index(Request $request)
+    {
+        return Inertia::render('Artikel/Index', [
+            'artikel' => Artikel::all(),
+        ]);
+    }
+
+    /**
+     * Display the user's profile form.
+     */
+    public function edit(Request $request): Response
+    {
+        return Inertia::render('Profile/Edit', [
+            'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
+            'status' => session('status'),
+        ]);
+    }
+
+    /**
+     * Update the user's profile information.
+     */
+    public function update(ProfileUpdateRequest $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit');
+    }
+
+    /**
+     * Delete the user's account.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'password' => ['required', 'current-password'],
+        ]);
+
+        $user = $request->user();
+
+        Auth::logout();
+
+        $user->delete();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return Redirect::to('/');
+    }
+
+    /**
+     * Delete the user's account.
+     */
+    public function show(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'artikelId' => 'required',
+        ]);
+
+        return Inertia::render('Artikel/Show', [
+            'artikel' => Artikel::whereLike('artikelId', $request->artikelId),
+        ]);
+    }
+}
